06140230ff6643e41e8a4ec390063304d81bdb2a
nadiashovkovy <nadiashovkovy@gmail.com>
2024-07-28 16:06:01 -0700

    removed pfp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
-import profileImage from '/Users/nadiashovkovy/Desktop/nadia-personal-site/src/assets/images/profile.png';  // Adjust the path as needed
+// import profileImage from '/Users/nadiashovkovy/Desktop/nadia-personal-site/src/assets/images/profile.png';  // Adjust the path as needed
+//<img src={profileImage} alt="Nadia Shovkovy" className="profile-image" />
 
 const Home = () => {
   return (
@@ -11,7 +12,6 @@ const Home = () => {
         <p>This is a place where I share my tech projects, my passions, and more.</p>
         <button className="cta-button">Learn more about me...</button>
       </div>
-      <img src={profileImage} alt="Nadia Shovkovy" className="profile-image" />
     </section>
   );
 }
