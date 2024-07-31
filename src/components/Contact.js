c69c4db4b8495d8e3925863d97d64d78e426217c
nadiashovkovy <nadiashovkovy@gmail.com>
2024-07-30 20:53:11 -0700

    major style changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,21 @@ import './Contact.css';
 const Contact = () => {
   return (
     <section id="contact" className="contact-section">
-      <h1>Let's connect.</h1>
-      <p>This is the contact section.</p>
+      <h1>Let's Connect.</h1>
+      <div className="contact-grid">
+      <a href="https://www.linkedin.com/in/nadia-shovkovy" target="_blank" rel="noopener noreferrer">
+        <img src="https://static-00.iconduck.com/assets.00/linkedin-icon-2048x2048-3mmtnow6.png" alt="LinkedIn" className="contact-logo" />
+      </a>
+      <a href="https://medium.com/@nadiashovkovy" target="_blank" rel="noopener noreferrer">
+        <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/medium-round-icon.png" alt="Medium" className="contact-logo" />
+      </a>
+      <a href="https://github.com/nadiashovkovy" target="_blank" rel="noopener noreferrer">
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" className="contact-logo" />
+      </a>
+      <a href="https://www.instagram.com/nadiashovkovy/" target="_blank" rel="noopener noreferrer">
+        <img src="https://static.vecteezy.com/system/resources/previews/022/498/176/non_2x/3d-render-camera-icon-isolated-on-transparent-background-camera-logo-for-web-site-app-ui-and-logo-free-png.png" alt="Instagram" className="contact-logo" />
+      </a>
+    </div>
     </section>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,12 +4,33 @@ import './Projects.css';
 const Projects = () => {
   return (
     <section id="projects" className="project-section">
-      <h1>My Projects</h1>
-      <div className="horizontal-scroll-container">
-        <div className="horizontal-scroll-content">
-          <div className="scroll-item">Volunteen</div>
-          <div className="scroll-item">Machine Learning Research</div>
-          <div className="scroll-item">Kids in Tech</div>
+      <h1>Some of my recent work...</h1>
+      <div className="my-projects">
+        <div className="project-item">
+          <h3>
+            VolunTeen
+          </h3>
+          <p>
+            VolunTeen is an iOS app connecting teen volunteers with community service events in Arizona.
+            I programmed the UI and the backend code on Xcode with Swift & SwiftUI. I also Implemented user authentication and in-app messaging using Firebase.
+          </p>
+        </div>
+        <div className="project-item">
+          <h3>
+            Machine Learning Research
+          </h3>
+          <p>
+            During my third year of high school, I wrote a transfer learning program with Mask R-CNN (Pre-trained with the COCO2017 dataset) using the Stanford Background Dataset and PyTorch.
+          </p>
+        </div>
+        <div className="project-item">
+          <h3>
+            Kids In Tech
+          </h3>
+          <p>
+            Kids In Tech Arizona is a volunteering organization dedicated to teaching engineering and coding at elementary schools. As a program founder, I designed and led workshops on block-coding, mBot, and Girls in Tech at Ryan Elementary.
+            I also helped to establish and coach Weinberg Elementary’s First Lego League Explore team.
+          </p>
         </div>
       </div>
     </section>
